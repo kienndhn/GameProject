21d96c57baaffb0ad295b16c462696c27a096059
nguyendinhkien <nguyendinhkien12344321@gmail.com>
2020-11-04 20:32:06 +0700

    add shader

--- TrainingFramework/src/GameStates/GSPlay.cpp
+++ TrainingFramework/src/GameStates/GSPlay.cpp
@@ -8,8 +8,7 @@
 #include "Sprite2D.h"
 #include "Sprite3D.h"
 #include "Text.h"
-
-#include "GameButton.h"
+#include "SpriteAnimation.h"
 
 extern int screenWidth; //need get on Graphic engine
 extern int screenHeight; //need get on Graphic engine
@@ -36,17 +35,6 @@ void GSPlay::Init()
 	m_BackGround->Set2DPosition(screenWidth / 2, screenHeight / 2);
 	m_BackGround->SetSize(screenWidth, screenHeight);
 
-	
-	
-	texture = ResourceManagers::GetInstance()->GetTexture("button_back");
-	std::shared_ptr<GameButton> button = std::make_shared<GameButton>(model, shader, texture);
-	button = std::make_shared<GameButton>(model, shader, texture);
-	button->Set2DPosition(240, 50);
-	button->SetSize(100, 50);
-	button->SetOnClick([]() {
-		GameStateMachine::GetInstance()->PopState();
-	});
-	m_listButton.push_back(button);
 
 	//text game title
 	shader = ResourceManagers::GetInstance()->GetShader("TextShader");
@@ -54,12 +42,12 @@ void GSPlay::Init()
 	m_score = std::make_shared< Text>(shader, font, "score: 10", TEXT_COLOR::RED, 1.0);
 	m_score->Set2DPosition(Vector2(5, 25));
 
-	//animation
+	// Animation
 	shader = ResourceManagers::GetInstance()->GetShader("Animation");
-	texture = ResourceManagers::GetInstance()->GetTexture("play_run");
-	std::shared_ptr<SpriteAnimation> obj = std::make_shared<SpriteAnimation>(model, shader, texture, 6, 0.3f);
-	obj->Set2DPosition(240, 180);
-	obj->SetSize(32, 32);
+	texture = ResourceManagers::GetInstance()->GetTexture("coin1");
+	std::shared_ptr<SpriteAnimation> obj = std::make_shared<SpriteAnimation>(model, shader, texture, 6, 0.1f);
+	obj->Set2DPosition(120, 120);
+	obj->SetSize(52, 52);
 	m_listSpriteAnimations.push_back(obj);
 }
 
@@ -87,16 +75,11 @@ void GSPlay::HandleEvents()
 
 void GSPlay::HandleKeyEvents(int key, bool bIsPressed)
 {
-	
+
 }
 
 void GSPlay::HandleTouchEvents(int x, int y, bool bIsPressed)
 {
-	for (auto it : m_listButton)
-	{
-		(it)->HandleTouchEvents(x, y, bIsPressed);
-		if ((it)->IsHandle()) break;
-	}
 }
 
 void GSPlay::Update(float deltaTime)
@@ -110,15 +93,13 @@ void GSPlay::Update(float deltaTime)
 void GSPlay::Draw()
 {
 	m_BackGround->Draw();
-	m_score->Draw();
-	for (auto it : m_listButton)
-	{
-		it->Draw();
-	}
+
 	for (auto obj : m_listSpriteAnimations)
 	{
 		obj->Draw();
 	}
+
+	m_score->Draw();
 }
 
 void GSPlay::SetNewPostionForBullet()
